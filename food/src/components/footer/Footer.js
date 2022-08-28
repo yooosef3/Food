d88b9f61998ade9426de4dbf667cbec8b25d0ed8
Yusef Khedri <yooosef.khedri@gmail.com>
2022-08-29 00:21:40 +0430

    footer section finished

--- food/src/components/footer/Footer.js
+++ food/src/components/footer/Footer.js
@@ -16,71 +16,75 @@ const Footer = () => {
   return (
     <footer>
       <div className={styles.container}>
-        <div>
-          <Link to='/'>
-            <img alt="logo" src={logo} />
-          </Link>
-          <div className={styles.visit}>
+        <div className={styles.twoCol}>
+          <div>
+            <Link to="/">
+              <img alt="logo" src={logo} />
+            </Link>
+            <div className={styles.visit}>
+              <div>
+                <strong>روزهای هفته: </strong>
+                <span>08:00am - 08:00pm</span>
+              </div>
+              <div>
+                <strong>آخر هفته: </strong>
+                <span>10:00am - 06:00pm</span>
+              </div>
+            </div>
+            <div className={styles.socials}>
+              <p>شبکه های اجتماعی</p>
+              <ul>
+                <li>
+                  <Link to="/">
+                    <FaFacebookF className={styles.icon} />
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/">
+                    <FaTwitter className={styles.icon} />
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/">
+                    <FaInstagram className={styles.icon} />
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/">
+                    <FaGooglePlusG className={styles.icon} />
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className={styles.contact}>
+            <h1>تماس با ما</h1>
             <div>
-              <strong>روزهای هفته: </strong>
-              <span>08:00am - 08:00pm</span>
+              <MdLocationPin className={styles.icon} />
+              <p>
+                ارومیه, بلوار والفجر - آخر خیابان میثم - کوی یازده - بن بست اول
+                , پلاک سه
+              </p>
             </div>
             <div>
-              <strong>آخر هفته: </strong>
-              <span>10:00am - 06:00pm</span>
+              <BsFillTelephoneFill className={styles.icon} />
+              <p>+98-914-968-9984</p>
             </div>
-          </div>
-          <div className={styles.socials}>
-            <p>شبکه های اجتماعی</p>
-            <ul>
-              <li>
-                <Link to="/">
-                  <FaFacebookF className={styles.icon} />
-                </Link>
-              </li>
-              <li>
-                <Link to="/">
-                  <FaTwitter className={styles.icon} />
-                </Link>
-              </li>
-              <li>
-                <Link to="/">
-                  <FaInstagram className={styles.icon} />
-                </Link>
-              </li>
-              <li>
-                <Link to="/">
-                  <FaGooglePlusG className={styles.icon} />
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className={styles.contact}>
-          <h1>تماس با ما</h1>
-          <div>
-            <MdLocationPin className={styles.icon} />
-            <p>
-              ارومیه, بلوار والفجر - آخر خیابان میثم - کوی یازده - بن بست اول ,
-              پلاک سه
-            </p>
-          </div>
-          <div>
-            <BsFillTelephoneFill className={styles.icon} />
-            <p>+98-914-968-9984</p>
-          </div>
-          <div>
-            <HiOutlineMail className={styles.icon} />
-            <p>Yusef_khedri@yahoo.com</p>
+            <div>
+              <HiOutlineMail className={styles.icon} />
+              <p>Yusef_khedri@yahoo.com</p>
+            </div>
           </div>
         </div>
         <div className={styles.newsletter}>
-          <h1>خبرنامه</h1>
-          <p>
-            برای دریافت اخبار هفتگی، به روز رسانی ها، پیشنهادات ویژه و تخفیف های
-            منحصر به فرد، در خبرنامه ما مشترک شوید.
-          </p>
-          <input type="text" placeholder="ایمیل خود را وارد کنید" />
+          <section>
+            <h1>خبرنامه</h1>
+            <p>
+              برای دریافت اخبار هفتگی، به روز رسانی ها، پیشنهادات ویژه و تخفیف
+              های منحصر به فرد، در خبرنامه ما مشترک شوید.
+            </p>
+            <input type="text" placeholder="ایمیل خود را وارد کنید" />
+          </section>
           <div>
             <IoIosSend />
           </div>
--- food/src/components/footer/FooterBotom.js
+++ food/src/components/footer/FooterBotom.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import pay1 from '../../assets/images/payment-1-45x15.png'
-import pay2 from '../../assets/images/payment-2-46x28.png'
-import pay3 from '../../assets/images/payment-3-62x17.png'
-import styles from '../../styles/footer/FooterBotom.module.css'
+import React from "react";
+import pay1 from "../../assets/images/payment-1-45x15.png";
+import pay2 from "../../assets/images/payment-2-46x28.png";
+import pay3 from "../../assets/images/payment-3-62x17.png";
+import styles from "../../styles/footer/FooterBotom.module.css";
 
 const FooterBotom = () => {
-    return (
-        <div>
-            <p>© 2022 Grocmart. تمامی حقوق محفوظ است</p>
-            <div>
-                <p>سیاست حفظ حریم خصوصی</p>
-                <div>
-                    <div>
-                        <img alt='payment' src={pay1}/>
-                    </div>
-                    <div>
-                        <img alt='payment' src={pay2}/>
-                    </div>
-                    <div>
-                        <img alt='payment' src={pay3}/>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <p>© 2022 code.load تمامی حقوق محفوظ است</p>
+      <div>
+        <p>سیاست حفظ حریم خصوصی</p>
+        <section>
+          <div>
+            <img alt="payment" src={pay1} />
+          </div>
+          <div>
+            <img alt="payment" src={pay2} />
+          </div>
+          <div>
+            <img alt="payment" src={pay3} />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default FooterBotom;
