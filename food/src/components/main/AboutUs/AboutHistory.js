848a9b5941e165a37702f75a66bbe441cd0003fc
Yusef Khedri <yooosef.khedri@gmail.com>
2022-09-12 22:00:02 +0430

    about history section finished

--- food/src/components/main/AboutUs/AboutHistory.js
+++ food/src/components/main/AboutUs/AboutHistory.js
@@ -1,10 +1,12 @@
+import AboutHistorySlider from './AboutHistorySlider';
 import React from 'react';
 import styles from '../../../styles/main/AboutUs/AboutHistory.module.css'
 
 const AboutHistory = () => {
     return (
         <div className={styles.container}>
-            
+            <h1>تاریخچه ما</h1>
+            <AboutHistorySlider />
         </div>
     );
 };
---
+++ food/src/components/main/AboutUs/AboutHistorySlider.js
@@ -0,0 +1,77 @@
+import "../../../styles/main/AboutUs/AboutHistorySlider.css";
+import "swiper/css";
+import "swiper/css/navigation";
+
+import { Autoplay, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import React from "react";
+
+const AboutHistorySlider = () => {
+  return (
+    <>
+      <Swiper
+        breakpoints={{
+          640: {
+            width: 640,
+            slidesPerView: 1,
+          },
+          768: {
+            width: 768,
+            slidesPerView: 2,
+          }
+        }}
+        navigation={true}
+        modules={[Navigation, Autoplay]}
+        spaceBetween={30}
+        loop={true}
+        autoplay={{
+          delay: 4000,
+        }}
+        className="aboutHistorySwiper"
+      >
+        <SwiperSlide className="first">
+          <h3>تاسیس</h3>
+          <p>
+            چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته
+            شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه رعایت
+            حق تکثیر متون را ندارند
+          </p>
+        </SwiperSlide>
+        <SwiperSlide className="second">
+          <h3>مشارکت اول</h3>
+          <p>
+            چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته
+            شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه رعایت
+            حق تکثیر متون را ندارند
+          </p>
+        </SwiperSlide>
+        <SwiperSlide className="third">
+          <h3>راه اندازی وب سایت</h3>
+          <p>
+            چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته
+            شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه رعایت
+            حق تکثیر متون را ندارند
+          </p>
+        </SwiperSlide>
+        <SwiperSlide className="forth">
+          <h3>افتتاح فروشگاه های جدید</h3>
+          <p>
+            چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته
+            شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه رعایت
+            حق تکثیر متون را ندارند
+          </p>
+        </SwiperSlide>
+        <SwiperSlide className="fifth">
+          <h3>جشن 20 سالگی</h3>
+          <p>
+            چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته
+            شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه رعایت
+            حق تکثیر متون را ندارند
+          </p>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+export default AboutHistorySlider;
