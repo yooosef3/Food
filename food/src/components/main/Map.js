7546de592b90319fc81cc8c8b17ae7d7ff50af05
Yusef Khedri <yooosef.khedri@gmail.com>
2022-09-12 18:44:53 +0430

    blogs section finished

--- food/src/components/main/Map.js
+++ food/src/components/main/Map.js
@@ -1,12 +1,18 @@
+import "leaflet/dist/leaflet.css";
+
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 import React from "react";
-import styles from '../../styles/main/Map.module.css'
 
 const Map = () => {
   return (
-    <div className={styles.container}>
-      <MapContainer center={[51.505, -0.09]} zoom={5} scrollWheelZoom={true}>
+    <div>
+      <MapContainer
+        center={[51.505, -0.09]}
+        style={{ height: "500px" }}
+        zoom={5}
+        scrollWheelZoom={true}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
